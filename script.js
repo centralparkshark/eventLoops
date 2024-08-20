a46a2069d17d78d17ff01c68b84e448bc40cc552
Vinncent Miller <vinncentmiller@gmail.com>
2024-08-20 12:11:35 -0400

    Event Loop Counter

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// Declare a global counter variable.
 let counter = 0;
-// Create a simple function that increments the variable, and then calls itself recursively.
-// Surround the initial function call in a try/catch block.
-// Within the catch, log the error and the value of the counter variable.
+
+function incrementCounter() {
+    counter++;
+    incrementCounter();
+}
+
 try {
-    addOne();
+    incrementCounter();
 } catch (error) {
     console.log(error);
-    console.log(counter)
+    console.log(counter);
 }
 
-function addOne() {
-    counter++;
-    //addOne();
-}
-
-// returns in the range of 28,000 - 37,000
+// InternalError: too much recursion
+// 31,000
